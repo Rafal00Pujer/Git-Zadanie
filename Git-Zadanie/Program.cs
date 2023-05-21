a9c9c2630a75e9a06cf79b7f523c9e7900a78df8
RafalPujer <rafal.pujer00@gmail.com>
2023-05-21 20:51:04 +0200

    initial commit

---
+++ Git-Zadanie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Git_Zadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
